56bb1154d330b4acc1f7e8db3b8347faac7fb2ae
Alex Chu <chu.alex9102@gmail.com>
2019-07-11 23:00:28 -0400

    fixed movie search bug and added default case for movies if no title input on cli

--- liri.js
+++ liri.js
@@ -42,7 +42,6 @@ var spotify = function() {
     input = process.argv;
     
     if (!input[3]) {
-        // songName = "The+Sign" 
         spotify.search({ type: 'track', query: "The Sign"})
         .then(function(response) {
         console.log("Artist: " + response.tracks.items[5].artists[0].name);
@@ -80,15 +79,47 @@ var spotify = function() {
 var movie = function() {
     axios = require("axios");
     input = process.argv;
-    movieName = ""; 
+
+    if (!input[3]) {
+        queryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy"
+        axios.get(queryUrl).then(
+            function(response) {
+              console.log("Title of the movie: " + response.data.Title);
+              console.log("Year of the movie came out: " + response.data.Year);
+              console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+              console.log("Country Produced: " + response.data.Country);
+              console.log("Language: " + response.data.Language);
+              console.log("Plot: " + response.data.Plot);
+              console.log("Actors: " + response.data.Actors);
+              console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+     
+            })
+            .catch(function(error) {
+                if(error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            })
+    } 
+    else {
+    movieName = process.argv.slice(3).join(" "); 
     
-    for (var i = 3; i < input.length; i++) {
-        if (i > 2 && i < input.length) {
-            movieName = movieName + "+" + input[i];
-          } else {
-            movieName += input[i];
-          }
-    }
+    // for (var i = 3; i < input.length; i++) {
+    //     if (i > 2 && i < input.length) {
+    //         movieName = movieName + "+" + input[i];
+    //       } else {
+    //         movieName += input[i];
+    //       }
+    // }
 
     queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
 
@@ -119,6 +150,7 @@ var movie = function() {
             }
             console.log(error.config);
         })
+    }
 };
 
 var doThis = function() {
